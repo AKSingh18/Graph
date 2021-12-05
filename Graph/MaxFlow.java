4777467fd8b5ff6dca4c6b2b4c896d7966b04ebd
AKSingh18 <anuj.anonymous@gmail.com>
2021-12-05 14:52:47 +0530

    Add push relabel algorithm for finding maximum flow.

--- Graph/MaxFlow.java
+++ Graph/MaxFlow.java
@@ -28,7 +28,7 @@ public class MaxFlow
                 // If forward edge already exists, update its weight
                 if (residualGraph.hasEdge(u, neighbour.destination))
                     residualGraph.getNeighbour(u, neighbour.destination).weight += neighbour.weight;
-                    // In case it does not exist, create one
+                // In case it does not exist, create one
                 else residualGraph.addEdge(u, neighbour.destination, neighbour.weight);
 
                 // If backward edge does not already exist, add it
@@ -115,5 +115,89 @@ public class MaxFlow
         // If queue is empty, then sink cannot be located
         if (queue.isEmpty()) return null;
         else return parent;
+    }
+
+    /**
+     *
+     * The method uses push-relabel algorithm to find max flow. It assumes 0 as source and vertices-1 as sink.
+     *
+     * Test Link: https://practice.geeksforgeeks.org/problems/find-the-maximum-flow2126/1
+     *
+     * @return maximum flow
+     */
+    public int pushRelabel()
+    {
+        Graph residualGraph = constructResidualGraph();
+
+        // Step 1: Initialize pre-flow
+        int[] e = new int[graph.vertices]; // to store excess flow
+        int[] h = new int[graph.vertices]; // to store height of vertices
+
+        h[0] = graph.vertices;
+
+        for (Graph.Neighbour neighbour: graph.adjacencyList.get(0))
+        {
+            residualGraph.getNeighbour(0, neighbour.destination).weight = 0;
+            residualGraph.getNeighbour(neighbour.destination, 0).weight = neighbour.weight;
+
+            e[neighbour.destination] = neighbour.weight;
+        }
+
+        // Step 2: Update the pre-flow while there remains an applicable push or relabel operation
+        int u = getOverflowVertex(e);
+        while (u != -1)
+        {
+            relabel(residualGraph, u, h);
+            push(residualGraph, u, e, h);
+
+            u = getOverflowVertex(e);
+        }
+
+        return e[graph.vertices-1];
+    }
+
+    /**
+     *
+     * @param e excess flow array
+     * @return overflow vertex index if found else -1 if there are no overflowing vertices
+     */
+    private int getOverflowVertex(int[] e)
+    {
+        for (int i = 1; i < graph.vertices-1; i++) if (e[i] > 0) return i;
+
+        return -1;
+    }
+
+    private void relabel(Graph residualGraph, int u, int[] h)
+    {
+        int minHeight = Integer.MAX_VALUE;
+
+        for (Graph.Neighbour neighbour: residualGraph.adjacencyList.get(u))
+        {
+            if (neighbour.weight > 0) minHeight = Math.min(h[neighbour.destination], minHeight);
+        }
+
+        h[u] = minHeight+1;
+    }
+
+    private void push(Graph residualGraph, int u, int[] e, int[] h)
+    {
+        for (Graph.Neighbour neighbour: residualGraph.adjacencyList.get(u))
+        {
+            // after pushing flow if there is no excess flow, then break
+            if (e[u] == 0) break;
+
+            // push more flow to the adjacent vertex if possible
+            if (neighbour.weight > 0 && h[neighbour.destination] < h[u])
+            {
+                int f = Math.min(e[u], neighbour.weight);
+
+                neighbour.weight -= f;
+                residualGraph.getNeighbour(neighbour.destination,u).weight += f;
+
+                e[u] -= f;
+                e[neighbour.destination] += f;
+            }
+        }
     }
 }
