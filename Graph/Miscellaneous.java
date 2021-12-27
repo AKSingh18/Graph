24169550f492d9b887f05f751a9050f1c6893029
AK <anuj.anonymous@gmail.com>
2021-12-27 16:46:56 +0530

    Remove method to find maximum matching

--- Graph/Miscellaneous.java
+++ Graph/Miscellaneous.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
  * Contains methods to find
  *     1: articulation points
  *     2: bridges
- *     3: maximum matching in bipartite graph
  */
 public class Miscellaneous
 {
@@ -129,71 +128,5 @@ public class Miscellaneous
             }
             else if (v.i != parent[source]) lowLink[source] = Math.min(lowLink[source], discovery[v.i]);
         }
-    }
-
-    /**
-     * The method uses edmond-karp algorithm to find maximum matching in the bipartite graph.
-     *
-     * The user need to use the undirected graph passed in the constructor parameter to represent the
-     * bipartite graph in question. The method will convert the undirected graph to flow graph with each edge
-     * having unit weight. The flow graph will be used in finding the maximum matching.
-     *
-     * It is necessary that the user represents their bipartite graph correctly using undirected graph.
-     * Here is how the undirected graph should be constructed:
-     *
-     * Labelling:
-     *     1: V       -> total number of vertices
-     *     2: L       -> number of vertices in the left set
-     *     3: 0-(L-1) -> value of each vertex in the set L
-     *     4: L-(V-1) -> value of each vertex in the set R
-     *
-     * Example graph:
-     *
-     *    [L]     [R]
-     *     0 ----- 3
-     *     1 ----- 4
-     *     2 ----- 5
-     *
-     * Corresponding flow graph:
-     *
-     *         1 -----> 4
-     *       /            \
-     *      /              \
-     *     0 - 2 -----> 5 - 7
-     *      \              /
-     *       \            /
-     *         3 -----> 6
-     *
-     * Source - 0 ... Sink - 7 ... each edge has unit weight
-     *
-     * Test Link: https://practice.geeksforgeeks.org/problems/maximum-bipartite-matching/1
-     *
-     * @param L number of vertices in the left set
-     * @return the maximum matching
-     */
-    public int maxMatchingBiGraph(int L)
-    {
-        final int source = 0;
-        final int sink = graph.vertices+1;
-
-        // Construct a new graph with source 0 and sink graph.vertices.
-        DirectedGraph flowGraph = new DirectedGraph(sink+1);
-
-        // Add edges from source to vertices of set L
-        for (int i = 1;i <= L;i++) flowGraph.addEdge(source, i, 1);
-
-        // Add edges from vertices of set R to sink
-        for (int i = L+1;i <= sink-1;i++) flowGraph.addEdge(i, sink, 1);
-
-        // Add all the edges belonging to the bipartite graph
-        for (int i = 1;i <= L;i++)
-        {
-            // A vertex i in the flow graph will be vertex i-1 in the original graph
-            for (Graph.Vertex v : graph.adjacencyList.get(i-1)) flowGraph.addEdge(i, v.i+1, 1);
-        }
-
-        // Run edmonds-karp on the flow graph to find the maximum matching
-        MaxFlow maxFlow = new MaxFlow(flowGraph, source, sink);
-        return maxFlow.edmondsKarp();
     }
 }
