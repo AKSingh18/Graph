8a117e0f486b5dfa931279afb50c7608c25738bf
AK <anuj.anonymous@gmail.com>
2021-12-30 20:17:56 +0530

    Add kruskals algorithm

--- Graph/MST.java
+++ Graph/MST.java
@@ -1,10 +1,15 @@
 package Graph;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+
+import static Graph.Graph.Vertex;
+import static Graph.Graph.Edge;
 
 public class MST
 {
-    private Graph graph;
+    private UndirectedGraph graph;
 
     public MST(UndirectedGraph graph)
     {
@@ -77,5 +82,86 @@ public class MST
         }
 
         return minIndex;
+    }
+
+    /**
+     *
+     * The following kruskals method uses union-find to detect cycle while constructing the MST
+     *
+     * Test Link: https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1#
+     *
+     * @return an integer denoting the sum of weights of the edges of the MST
+     */
+    public int kruskals()
+    {
+        // Step 1: Construct edges array consisting of all the edges of the input graph
+        ArrayList<Edge> edges = new ArrayList<>();
+        UndirectedGraph tempUG = new UndirectedGraph(graph);
+
+        for (int u = 0;u < tempUG.vertices;u++)
+        {
+            ArrayList<Vertex> vertices = tempUG.adjacencyList.get(u);
+
+            while (!vertices.isEmpty())
+            {
+                /* In case of undirected graph, an edge from u-v will be present two times. Once in,
+                   neighbouring vertices of u and other in neighbouring vertices of v. Since, it should
+                   be traversed only one time. The latter occurrence will be removed.
+
+                    v:  u->v
+                   _v:  v->u
+                */
+
+                Vertex v = vertices.remove(vertices.size()-1);
+                Vertex _v = tempUG.getNeighbour(v.i, u);
+
+                tempUG.adjacencyList.get(v.i).remove(_v);
+
+                // Add the edge to the list
+                edges.add(new Edge(u, v.i, v.w));
+            }
+        }
+
+        // Step 2: Sort the edges based on their weights
+        edges.sort(new Comparator<>()
+        {
+            @Override
+            public int compare(Edge e1, Edge e2) {
+                return Integer.compare(e1.w, e2.w);
+            }
+        });
+
+        // Step 3: Keep picking the edge which do not form a cycle
+        int[] parent = new int[graph.vertices];
+        Arrays.fill(parent, -1);
+
+        int weight = 0;
+
+        for (Edge e : edges)
+        {
+            if (find(parent, e.u) != find(parent, e.v))
+            {
+                union(parent, e.u, e.v);
+                weight += e.w;
+            }
+        }
+
+        return weight;
+    }
+
+    // Makes parent of u the parent of v
+    private void union(int[] parent, int u, int v)
+    {
+        int x = find(parent, u);
+        int y = find(parent, v);
+
+        parent[y] = x;
+    }
+
+    // Returns the root parent of i
+    private int find(int[] parent, int i)
+    {
+        if (parent[i] == -1) return i;
+        else return find(parent, parent[i]);
     }
 }
